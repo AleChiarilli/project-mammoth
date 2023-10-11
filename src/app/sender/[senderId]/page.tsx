f84f1afc34abf47659aa8c751aad8167db5d9ee2
AleChiarilli <alejandrochiarilli@gmail.com>
2023-10-11 19:00:06 +0200

    feat: add Tabs categories

--- src/app/sender/[senderId]/page.tsx
+++ src/app/sender/[senderId]/page.tsx
@@ -151,7 +151,6 @@ export default function SenderView({
           </Button>
         </div>
       </div>
-
       <TabsCategories />
     </>
   );
--- src/app/sender/components/sections.tsx
+++ src/app/sender/components/sections.tsx
@@ -1,25 +1,33 @@
+"use client";
 import { Tabs, Box, Text } from "@radix-ui/themes";
 
 export default function TabsCategories() {
   return (
     <>
-      <Tabs.Root defaultValue="account">
-        <Tabs.List>
-          <Tabs.Trigger value="account">Account</Tabs.Trigger>
-          <Tabs.Trigger value="documents">Documents</Tabs.Trigger>
-          <Tabs.Trigger value="settings">Settings</Tabs.Trigger>
+      <Tabs.Root defaultValue="platillos">
+        <Tabs.List className="flex justify-between px-20">
+          <Tabs.Trigger value="platillos">PLATILLOS</Tabs.Trigger>
+          <Tabs.Trigger value="bebidas">BEBIDAS</Tabs.Trigger>
+          <Tabs.Trigger value="postres">POSTRES</Tabs.Trigger>
+          <Tabs.Trigger value="extras">EXTRAS</Tabs.Trigger>
         </Tabs.List>
 
         <Box px="4" pt="3" pb="2">
-          <Tabs.Content value="account">
+          <Tabs.Content value="platillos">
             <Text size="2">Make changes to your account.</Text>
           </Tabs.Content>
 
-          <Tabs.Content value="documents">
+          <Tabs.Content value="bebidas">
             <Text size="2">Access and update your documents.</Text>
           </Tabs.Content>
 
-          <Tabs.Content value="settings">
+          <Tabs.Content value="postres">
+            <Text size="2">
+              Edit your profile or update contact information.
+            </Text>
+          </Tabs.Content>
+
+          <Tabs.Content value="extras">
             <Text size="2">
               Edit your profile or update contact information.
             </Text>
